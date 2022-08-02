5eb5b699a3ba7fb21bada7e923ec2a660bb96cea
Sourcery AI <>
2022-08-02 19:23:27 +0000

    'Refactored by Sourcery'

--- sources/tokeng.py
+++ sources/tokeng.py
@@ -10,29 +10,33 @@ import sys
 generator = random.SystemRandom ()
 
 
-def generate_token (schema, pattern_identifier, pattern_break) :
+def generate_token(schema, pattern_identifier, pattern_break):
 	while pattern_identifier in schema["aliases"] :
 		pattern_identifier = schema["aliases"][pattern_identifier]
 	pattern_groups = generate_groups (schema, pattern_identifier)
 	pattern_break = generate_break (schema, pattern_identifier, pattern_break)
 	token_glyphs = []
-	for pattern_group in pattern_groups :
-		if pattern_group == "-" :
+	for pattern_group in pattern_groups:
+		if pattern_group == "-":
 			token_glyphs.append (pattern_break)
 		elif pattern_group.startswith (":") :
 			token_glyphs.append (pattern_group[1:])
-		elif pattern_group.startswith ("!") :
-			for token in generate_token (schema, pattern_group[1:], pattern_break) :
-				token_glyphs.append (token)
-		else :
+		elif pattern_group.startswith ("!"):
+			token_glyphs.extend(
+				iter(generate_token(schema, pattern_group[1:], pattern_break))
+			)
+
+		else:
 			token_group = generate_group (schema, pattern_identifier, pattern_group)
 			token_glyph = generate_glyph (token_group)
 			token_glyphs.append (token_glyph)
-	token = "".join (token_glyphs)
-	return token
+	return "".join (token_glyphs)
 
-def generate_tokens (schema, pattern_identifier, pattern_break, count) :
-	return [generate_token (schema, pattern_identifier, pattern_break) for index in xrange (count)]
+def generate_tokens(schema, pattern_identifier, pattern_break, count):
+	return [
+		generate_token(schema, pattern_identifier, pattern_break)
+		for _ in xrange(count)
+	]
 
 
 def generate_groups (schema, pattern_identifier) :
@@ -52,8 +56,8 @@ def generate_group (schema, pattern_identifier, pattern_group) :
 		raise Exception ()
 
 
-def generate_break (schema, pattern_identifier, pattern_break) :
-	pattern_break = schema["breaks"].get (pattern_identifier + "*", pattern_break)
+def generate_break(schema, pattern_identifier, pattern_break):
+	pattern_break = schema["breaks"].get(f"{pattern_identifier}*", pattern_break)
 	if pattern_break is True :
 		return schema["breaks"][pattern_identifier]
 	elif pattern_break is False :
@@ -78,7 +82,7 @@ def display_token (stream, token, separator) :
 		stream.write (separator)
 
 
-def display_patterns (stream) :
+def display_patterns(stream):
 	trim_pattern = True
 	trim_pattern_length = 20
 	trim_token = True
@@ -86,19 +90,16 @@ def display_patterns (stream) :
 	show_pattern = False
 	identifiers = schema["patterns"].keys ()
 	identifiers.sort ()
-	for identifier in identifiers :
+	for identifier in identifiers:
 		pattern = schema["patterns"][identifier]
 		length = 0
 		strength = 1.0
-		for group in pattern :
-			if group == "-" :
+		for group in pattern:
+			if group == "-":
 				pass
 			elif group.startswith (":") :
 				pass
-			elif group.startswith ("!") :
-				# FIXME:  Should compute recursive pattern strength!
-				pass
-			else :
+			elif not group.startswith("!"):
 				group = generate_group (schema, identifier, group)
 				length += 1
 				strength *= len (group)
@@ -108,8 +109,8 @@ def display_patterns (stream) :
 		else :
 			year = -1
 		pattern = " ".join (pattern)
-		if trim_pattern and len (pattern) > trim_pattern_length :
-			pattern = pattern[:(trim_pattern_length - 4)] + " ..."
+		if trim_pattern and len (pattern) > trim_pattern_length:
+			pattern = f"{pattern[:(trim_pattern_length - 4)]} ..."
 		token = generate_token (schema, identifier, True)
 		if trim_token and len (token) > trim_token_length :
 			token = token[:(trim_token_length - 4)] + " ..."
@@ -120,19 +121,19 @@ def display_patterns (stream) :
 		stream.write (line)
 
 
-def display_glyphs (stream) :
+def display_glyphs(stream):
 	trim_example = True
 	trim_example_length = 40
 	identifiers = schema["glyphs"].keys ()
 	identifiers.sort ()
-	for identifier in identifiers :
+	for identifier in identifiers:
 		group = generate_group (schema, None, identifier)
 		length = len (group)
 		strength = length
 		strength = math.log (strength, 2)
 		example = str (group)
-		if trim_example and len (example) > trim_example_length :
-			example = example[:(trim_example_length - 4)] + " ..."
+		if trim_example and len (example) > trim_example_length:
+			example = f"{example[:(trim_example_length - 4)]} ..."
 		line = "| %-16s | %5d (%5.1f bits) | %s\n" % (identifier, length, strength, example)
 		stream.write (line)
 
@@ -184,13 +185,13 @@ def load_schema () :
 		return json.load (stream)
 
 
-if __name__ == "__main__" :
-	
+if __name__ == "__main__":
+
 	default_pattern_identifier = "p-aa-4"
 	default_pattern_count = 4
 	default_pattern_break = True
 	default_token_separator = "\n"
-	
+
 	if len (sys.argv) == 1 :
 		command = "one"
 		arguments = []
@@ -199,52 +200,48 @@ if __name__ == "__main__" :
 		arguments = sys.argv[2:]
 	else :
 		raise Exception ()
-	
+
 	schema = load_schema ()
-	
-	if command == "generate" and len (arguments) <= 4 :
+
+	if command == "generate" and len (arguments) <= 4:
 		pattern_identifier = arguments[0] if len (arguments) >= 1 else default_pattern_identifier
 		pattern_count = int (arguments[1]) if len (arguments) >= 2 else default_pattern_count
 		pattern_break = arguments[2] if len (arguments) >= 3 else default_pattern_break
-		if pattern_break == 't' :
+		if pattern_break == 't':
 			pattern_break = True
-		elif pattern_break == 'f' :
+		elif pattern_break == 'f' or pattern_break != 's' and pattern_break == '':
 			pattern_break = False
-		elif pattern_break == 's' :
+		elif pattern_break == 's':
 			pattern_break = ' '
-		elif pattern_break == '' :
-			pattern_break = False
-		else :
-			pass
 		token_separator = arguments[3] if len (arguments) >= 4 else default_token_separator
 		tokens = generate_tokens (schema, pattern_identifier, pattern_break, pattern_count)
 		display_tokens (sys.stdout, tokens, token_separator)
-		
+
 	elif command == "one" and len (arguments) <= 1 :
 		pattern_identifier = arguments[0] if len (arguments) >= 1 else default_pattern_identifier
 		token = generate_token (schema, pattern_identifier, False)
 		display_token (sys.stdout, token, default_token_separator)
-		
+
 	elif command == "one-n" and len (arguments) <= 1 :
 		pattern_identifier = arguments[0] if len (arguments) >= 1 else default_pattern_identifier
 		token = generate_token (schema, pattern_identifier, False)
 		display_token (sys.stdout, token, None)
-		
+
 	elif (command in schema["patterns"] or command in schema["aliases"]) and len (arguments) == 0 :
 		tokens = generate_token (schema, command, False)
 		display_token (sys.stdout, tokens, default_token_separator)
-		
+
 	elif command == "patterns" and len (arguments) == 0 :
 		display_patterns (sys.stdout)
-		
+
 	elif command == "glyphs" and len (arguments) == 0 :
 		display_glyphs (sys.stdout)
-		
+
 	elif command == "help" :
 		display_help (sys.stderr)
-		
-	else :
+
+	else:
 		display_help (sys.stderr)
 		sys.exit (1)
-	
+
 	sys.exit (0)
